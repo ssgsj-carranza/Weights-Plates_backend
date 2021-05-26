c7869be19a2785d8280be53c2ddb8427c373e628
unknown <ssgsj.carranza@gmail.com>
2021-05-26 12:08:40 -0500

    added list of supplements to the database with their name, description and side effect, tested end points to get all supplements and supplement by ID, 200 ok, back end seems to be done for now

--- weightsplates/supplements/models.py
+++ weightsplates/supplements/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Supplements(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=300)
-    side_effects = models.CharField(max_length=300)
+    description = models.CharField(max_length=500)
+    side_effects = models.CharField(max_length=500)
